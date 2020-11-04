6cd8694fa4770ebd20fdb1b33c790b2e50f98883
rishika1101 <73646361+rishika1101@users.noreply.github.com>
2020-11-04 17:05:55 +0530

    Add files via upload

---
+++ 04.11.20/DisplayNumbers.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class DisplayNumbers{
+public void main(String args[]){
+int i=1,n=8;
+while(i<=n)
+{
+System.out.println("i");
+i++;
+}
+}
+}
---
+++ 04.11.20/Numbers.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Natural Numbers
+{
+public static void main(String args[])
+{
+int sum=0;
+int n=100;
+for(i=n;i>=1;i--)
+{
+sum=sum+i;
+}
+System.out.print("sum=sum+i");
+}
+}
