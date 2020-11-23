e19f12e45c633bdb760863839a4a059b3e0b59fd
rishika1101 <73646361+rishika1101@users.noreply.github.com>
2020-11-24 00:32:35 +0530

    Add files via upload

---
+++ 23.11.2020/Eg.java
@@ -0,0 +1,13 @@
+class Solution{
+   static int num;
+   static String str;
+   static{
+      num = 97;
+      str = "Static keyword in Java";
+   }
+   public static void main(String args[])
+   {
+      System.out.println("Value of num: "+num);
+      System.out.println("Value of str: "+str);
+   }
+}
